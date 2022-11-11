57d84b09f8996f8ab98bef51e8e7c88753142e13
mirrer <alsejr1004@gmail.com>
2022-11-11 23:34:03 +0900

    Add: TIL 20221111

--- Baekjoon/함수/b4673/app.js
+++ Baekjoon/함수/b4673/app.js
@@ -1,10 +1,18 @@
-function solution() {
-  for (let i = 0; i < 20; i++) {
-    const num = i.toString().split('').map((v) => +v);
-    const sum = num.reduce((a, b) => a + b, 0);
-    
-    console.log(num, sum);
-  }
+function solution(num) {    
+  const sum = num.split('').map((v) => +v).reduce((a, b) => a + b, 0);  
+  num = +num;
+
+  return sum + num;  
 }
 
-console.log(solution());
+let arr = Array(10000).fill().map((v, i) => i + 1);
+for (let i = 1; i < 10000; i++) {  
+  let result = solution(String(i)); 
+
+  if (result <= 10000) {
+    let newArr = arr.filter((v) => v !== result);
+    arr = newArr;
+  }  
+}
+
+arr.map((v) => console.log(v));
