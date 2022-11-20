a9dec29cffda524c1e47d0fe444488f9abf156f5
mirrer <alsejr1004@gmail.com>
2022-11-21 00:41:22 +0900

    Fix: TIL 20221120

--- Baekjoon/기본수학2/b2581/app.js
+++ Baekjoon/기본수학2/b2581/app.js
@@ -2,26 +2,25 @@ const fs = require('fs');
 const filePath = process.platform === 'linux' ? '/dev/stdin' : 'input.txt';
 const input = fs.readFileSync(filePath).toString().split('\n');
 
-let a = Number(input[0]);
-let b = Number(input[1]);
-let primeNumbers = [];
-let sum = 0;
+let start = +input[0];
+let end = +input[1];
+const result = [];
 
-for(let target = a; target <= b; target++){
-    for(let p = 2; p <= target; p++){
-        if(p*p > target){
-            primeNumbers.push(target);
-            sum += target;
-            break;
-        }
-        if(target % p === 0){
-            break;
-        }
+for(let i = start; i <= end; i++){
+  for (let j = 2; j <= i; j++) {
+    if (j * j > i) {
+      result.push(i);
+      break;
     }
+    if (i % j === 0) {
+      break;
+    }
+  }
 }
-if(primeNumbers.length === 0){
-    console.log(-1);
-}else{
-    console.log(sum);
-    console.log(primeNumbers[0]);
+
+if (result.length === 0) {
+  console.log(-1);
+} else {
+  console.log(result.reduce((a, b) => a + b, 0));
+  console.log(result[0]);
 }
