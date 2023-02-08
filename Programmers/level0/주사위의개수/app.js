e9659145340c92902868b7902712ff26f246bc29
mirrer <alsejr1004@gmail.com>
2023-02-09 00:29:19 +0900

    Add: TIL 20230209

--- Programmers/level0/주사위의개수/app.js
+++ Programmers/level0/주사위의개수/app.js
@@ -1,3 +1,7 @@
 function solution(my_string) {
   return my_string.split('').filter(v => !isNaN(v)).map(v => +v).sort((a, b) => a - b);
 }
+
+function solution(box, n) {
+  return box.map(v => Math.floor(v / n)).reduce((a, b) => a * b, 1);
+}
