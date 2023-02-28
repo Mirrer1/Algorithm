b6a27bdba0bb597fb9bf35ed9cd3058e430c0b0d
mirrer <alsejr1004@gmail.com>
2023-02-28 11:10:25 +0900

    Fix: TIL 20230228

--- Programmers/level0/외계어사전/app.js
+++ Programmers/level0/외계어사전/app.js
@@ -1,3 +1,3 @@
 function solution(spell, dic) {    
-  return dic.some(v => spell.sort().toString() == [...v].sort().toString()) ? 1 : 2;
+  return dic.some(v => spell.sort().join('') === [...v].sort().join('')) ? 1 : 2;
 }
