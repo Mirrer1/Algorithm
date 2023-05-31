5d3a7293c391378c36149dc37b74c51949d79465
mirrer <alsejr1004@gmail.com>
2023-05-31 12:25:31 +0900

    Add: TIL 20230531

--- Programmers/level2/괄호_회전하기/app.js
+++ Programmers/level2/괄호_회전하기/app.js
@@ -1,33 +1,24 @@
-// 나의 풀이
-const gcd = (a, b) => {
-  while (b > 0) {
-    let temp = b;
-    b = a % b;
-    a = temp;
+function solution(s) {
+  const stack = [];
+  let result = 0;
+  let isRight = true;
+  if (s.length % 2 === 1) return 0;
+
+  for (let i = 0; i < s.length; i++){
+      let candidate = s.slice(i) + s.slice(0, i);
+      isRight = true;
+      for (let word of candidate) {
+          if (word === "(" || word === "{" || word === "[") stack.push(word);
+          else {
+              let leftWord = stack.pop();
+              if (word === ")" && leftWord === "(") continue;
+              if (word === "}" && leftWord === "{") continue;
+              if (word === "]" && leftWord === "[") continue;
+              isRight = false;
+              break;
+          }
+      }
+      if (isRight) result++;
   }
-  return a;
-};
-
-const lcm = (a, b) => {
-  return (a * b) / gcd(a, b);
-};
-
-const solution = arr => {
-  let answer = 1;
-
-  for (let i = 0; i < arr.length; i++) {
-    answer = lcm(answer, arr[i]);
-  }
-
-  return answer;
-};
-
-// 다른 사람의 풀이
-// 삼항 연산자와 재귀함수를 사용하여 간결하게 풀이
-const GCD = (a, b) => a % b ? GCD(b, a % b) : b;
-const LCM = (a, b) => a * b / GCD(a, b);
-
-function solution(arr) {
-  return arr.reduce((ac, v) => LCM(ac, v));
+  return result;
 }
-
