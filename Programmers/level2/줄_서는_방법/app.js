082f6133eb092b2100f8fabab65c84c2b9472ea6
mirrer <alsejr1004@gmail.com>
2023-07-15 12:20:06 +0900

    Add: TIL 20230715

---
+++ Programmers/level2/줄_서는_방법/app.js
@@ -0,0 +1,25 @@
+function solution(n, k) {
+  const base = Array.from({ length: n }, (_, i) => i + 1);
+  var answer = [];
+  k -= 1;
+
+  while (true) {
+    n--;
+    if (k === 0) {
+      answer.push(...base);
+      break;
+    }
+
+    const index = Math.floor(k / factorial(n));
+    k = k % factorial(n);
+    answer.push(base[index]);
+    base.splice(index, 1);
+  }
+
+  return answer;
+}
+
+const factorial = num => {
+  if (num === 0) return 1;
+  return num * factorial(num - 1);
+};
